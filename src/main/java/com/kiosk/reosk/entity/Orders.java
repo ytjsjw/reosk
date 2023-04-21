0a0bf92c59d4767b0ef677cd7c9205e295098325
onlyj <tlawltjr23@gmail.com>
2023-04-21 16:23:42 +0900

    0421

--- src/main/java/com/kiosk/reosk/entity/Orders.java
+++ src/main/java/com/kiosk/reosk/entity/Orders.java
@@ -1,17 +1,35 @@
 package com.kiosk.reosk.entity;
 
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import java.util.Date;
+import org.springframework.data.annotation.CreatedDate;
 
-public class Orders {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+public class Orders implements Serializable {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orders_seq")
+    @SequenceGenerator(name = "orders_seq", sequenceName = "orders_seq", allocationSize = 1)
     private int oNum;
-    private Date oDate;
+
+    @Id
+    @CreatedDate
+    @Column(name = "oDate")
+    private LocalDateTime oDate;
     private int stock;
     private long totalPrice;
 
+    private long oPrice;
+    private String oName;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.oDate = LocalDateTime.now();
+    }
+
     @ManyToOne
-    private Product code, name, price;
+    private Product product;
 
 }
--- src/main/java/com/kiosk/reosk/entity/Product.java
+++ src/main/java/com/kiosk/reosk/entity/Product.java
@@ -1,7 +1,9 @@
 package com.kiosk.reosk.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Entity
 public class Product {
 
     @Id
